5e8cfc4f50632e22086b298d3ea41fc1b050345c
Larko <larionov1001@gmail.com>
2023-10-28 10:55:00 +0300

    Position model

--- sites/django_sites/FirstSite/Main/models.py
+++ sites/django_sites/FirstSite/Main/models.py
@@ -1,12 +1,22 @@
 from django.db.models import *
 
 
+class Position(Model):
+    name = CharField(max_length=40, null=True, blank=True)
+    description = TextField(max_length=500)
+    min_expirience = IntegerField()
+
+    def __str__(self):
+        return f"{self.name}"
+
+
 class Candidate(Model):
     surname = CharField(max_length=40)
     age = IntegerField(default=20)
     test_task_mark = IntegerField(null=True, blank=True)
     interview_mark = IntegerField(null=True, blank=True)
     salary_exp = IntegerField(null=True, blank=True)
+    position = ForeignKey(Position, on_delete=PROTECT)
 
     def __str__(self):
         return f"{self.surname} ({self.age})"
